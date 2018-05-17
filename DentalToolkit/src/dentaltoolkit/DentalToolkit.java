8d7cbff4aac0062ea311b259712f66193778f810
erictoman <ericto117@gmail.com>
2018-05-16 21:57:27 -0500

    INICIAL
    LAST

---
+++ DentalToolkit/src/dentaltoolkit/DentalToolkit.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dentaltoolkit;
+
+import Base.MysqlConnect;
+import dentaltoolkit.Frame.LogIn;
+import java.io.IOException;
+import scanner.WebcamQRCodeExample;
+
+/**
+ *
+ * @author EHef_
+ */
+public class DentalToolkit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        MysqlConnect C=MysqlConnect.getDbCon();
+        LogIn L = new LogIn();
+        L.show();
+        //WebcamQRCodeExample a = new WebcamQRCodeExample();
+    }
+    
+}
